36dddc28d04f82038c4fd90e15cd647e9b953219
SHESHKUMAR95 <148970064+SHESHKUMAR95@users.noreply.github.com>
2023-11-30 12:57:41 +0530

    Initial commit

---
+++ 4Operation_Dividation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4Operation_Dividation
+{
+    internal class Program
+    {
+        public void Dividation()
+        {
+            int a = 100, b = 5;
+            int c = a / b;
+            Console.WriteLine("Dividation : " + c);
+        }
+        static void Main(string[] args)
+        {
+            Program op = new Program();
+            op.Dividation();
+            Console.ReadLine();
+        }
+    }
+}
